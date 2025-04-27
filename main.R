0cdecba4969f0e8f504593dd7fafdf7de0f84cb4
gbengasaolu <92299950+gbengasaolu@users.noreply.github.com>
2025-04-27 07:41:54 -0400

    made some updates

--- main.R
+++ main.R
@@ -1,9 +1,21 @@
 library(tidyverse)
 library(here)
+library(dplyr)
+
+gss_cat
+
 
 tv_hours_table <- gss_cat %>%
   filter (age<30) %>%
   group_by(marital) %>%
   summarise(mean_tv_hours=mean(tvhours, na.rm = T))
 
+
+tv_hours_table <- gss_cat%>%
+  filter(age>=30) %>%
+  group_by(marital) %>%
+  summarise(mean_tv_hours=mean(tvhours, na.rm = T))
+
 write_csv(tv_hours_table, here("TV_hours_by_marital.csv"))
+
+view("TV_hours_by_marital.csv")
